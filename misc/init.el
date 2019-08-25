59b1c1530930229e129b863c6e950aa09e112767
Abhijat Malviya <malviya.abhijat@gmail.com>
2019-08-25 12:38:23 +0530

    added init.el for reference

---
+++ misc/init.el
@@ -0,0 +1,28 @@
+(package-initialize)
+
+(setq package-archives
+      '(("gnu" . "http://elpa.gnu.org/packages/")
+	("melpa" . "http://melpa.org/packages/")))
+
+
+(global-company-mode)
+(global-linum-mode)
+(ido-mode t)
+
+(add-hook 'cider-repl-mode-hook #'cider-company-enable-fuzzy-completion)
+(add-hook 'cider-mode-hook #'cider-company-enable-fuzzy-completion)
+(add-hook 'cider-mode-hook 'eldoc-mode)
+
+(setq company-idle-delay 0.1)
+(setq company-minimum-prefix-length 2)
+
+(setq make-backup-files nil)
+(setq auto-save-default nil)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (magit company cider))))
+
