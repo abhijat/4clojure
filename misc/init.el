8f61f37243501a72c36c1653b1005aab54f8bd60
Abhijat Malviya <abhijat@ridecell.com>
2019-08-26 09:48:25 +0530

    changes to init.el

--- misc/init.el
+++ misc/init.el
@@ -5,13 +5,22 @@
 	("melpa" . "http://melpa.org/packages/")))
 
 
+(show-paren-mode 1)
 (global-company-mode)
 (global-linum-mode)
 (ido-mode t)
 
 (add-hook 'cider-repl-mode-hook #'cider-company-enable-fuzzy-completion)
+
 (add-hook 'cider-mode-hook #'cider-company-enable-fuzzy-completion)
 (add-hook 'cider-mode-hook 'eldoc-mode)
+(add-hook 'cider-mode-hook 'paredit-mode)
+(add-hook 'cider-mode-hook 'rainbow-delimiters-mode)
+
+(add-hook 'clojure-mode-hook #'cider-company-enable-fuzzy-completion)
+(add-hook 'clojure-mode-hook 'eldoc-mode)
+(add-hook 'clojure-mode-hook 'paredit-mode)
+(add-hook 'clojure-mode-hook 'rainbow-delimiters-mode)
 
 (setq company-idle-delay 0.1)
 (setq company-minimum-prefix-length 2)
@@ -24,5 +33,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (magit company cider))))
-
+ '(package-selected-packages (quote (magit company cider paredit rainbow-delimiters))))
